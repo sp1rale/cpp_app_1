4ada998ced9d32ce8872b0f7ddf54f104fe72a42
sp1rale <dimayaremchyk24@gmail.com>
2023-10-19 15:45:19 +0300

    done

--- main.cpp
+++ main.cpp
@@ -4,15 +4,11 @@ using namespace std;
 
 int main()
 {
-	double R, R0, R1, R2, R3;
-	cout << "R1: ";
-	cin >> R1;
-	cout << "R2: ";
-	cin >> R2;
-	cout << "R3: ";
-	cin >> R3;
-	R0 = 1 / R1 + 1 / R2 + 1 / R3;
-	R = 1 / R0;
-	cout << "R: " << R;
-	return 0;
+double L, R, S;
+cout << "L: ";
+cin >> L;
+R = L / (2 * 3.14);
+S = 3.14* pow(R,2);
+cout << "S: " << S;
+return 0;
 }
