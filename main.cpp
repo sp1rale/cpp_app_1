e14e3ed796915a73220e3a688869015294a27a43
sp1rale <dimayaremchyk24@gmail.com>
2023-10-27 17:51:31 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,18 +1,40 @@
 #include <iostream>
-#include <cmath>
+#include <Windows.h>
 using namespace std;
 
 int main()
 {
-	double R, R0, R1, R2, R3;
-	cout << "R1: ";
-	cin >> R1;
-	cout << "R2: ";
-	cin >> R2;
-	cout << "R3: ";
-	cin >> R3;
-	R0 = 1 / R1 + 1 / R2 + 1 / R3;
-	R = 1 / R0;
-	cout << "R: " << R;
+	double length, width, height, volume;
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, 10);
+	cout << "---------------------------------------------\n";
+	SetConsoleTextAttribute(hConsole, 13);
+	cout << "Calculation of the volume of a parallelepiped\n";
+	SetConsoleTextAttribute(hConsole, 10);
+	cout << "---------------------------------------------\n";
+	SetConsoleTextAttribute(hConsole, 13);
+	cout << "Enter the output data:\n";
+	SetConsoleTextAttribute(hConsole, 10);
+	cout << "---------------------------------------------\n";
+	SetConsoleTextAttribute(hConsole, 11);
+	cout << "length(cm)-> ";
+	SetConsoleTextAttribute(hConsole, 11);
+	cin >> length;
+	SetConsoleTextAttribute(hConsole, 11);
+	cout << "width(cm)-> ";
+	SetConsoleTextAttribute(hConsole, 11);
+	cin >> width;
+	cout << "height(cm)-> ";
+	SetConsoleTextAttribute(hConsole, 11);
+	cin >> height;
+	volume = length * width * height;
+	SetConsoleTextAttribute(hConsole, 10);
+	cout << "---------------------------------------------\n";
+	SetConsoleTextAttribute(hConsole, 13);
+	cout << "The volume is " << volume << endl;
+	SetConsoleTextAttribute(hConsole, 10);
+	cout << "---------------------------------------------\n";
+	SetConsoleTextAttribute(hConsole, 7);
+   
 	return 0;
 }
