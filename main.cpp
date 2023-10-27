92c2896ecff86088709732f9757bad6e41046ec7
sp1rale <dimayaremchyk24@gmail.com>
2023-10-27 16:30:01 +0300

    done

--- main.cpp
+++ main.cpp
@@ -4,15 +4,17 @@ using namespace std;
 
 int main()
 {
-	double R, R0, R1, R2, R3;
-	cout << "R1: ";
-	cin >> R1;
-	cout << "R2: ";
-	cin >> R2;
-	cout << "R3: ";
-	cin >> R3;
-	R0 = 1 / R1 + 1 / R2 + 1 / R3;
-	R = 1 / R0;
-	cout << "R: " << R;
+	int year;
+	cout << "Enter year: ";
+	cin >> year;
+	if ((year % 4 == 0 && year % 100 != 0 || year % 400 == 0)) {
+		cout << "This year has 366 days" << endl;
+	}
+	else
+	{
+				cout << "This year has 365 days" << endl;
+	}
+	
+
 	return 0;
 }
